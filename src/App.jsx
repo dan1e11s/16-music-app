92d9dd13ab1b0278b2dadab3c12b2ecad51d8c18
Daniel <danielmuratov2003@gmail.com>
2023-11-24 16:24:44 +0600

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainPage from './pages/MainPage/MainPage';
+import style from './global.module.scss';
+
+function App() {
+  return (
+    <div className={style.wrapper}>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,18 @@
+import tracksList from '../../assets/tracksList';
+import style from './mainPage.module.scss';
+import Track from '../../components/Track/Track';
+
+const MainPage = () => {
+  return (
+    <div className={style.search}>
+      <>Поиск треков</>
+      <div className={style.list}>
+        {tracksList.map((track) => (
+          <Track key={track.id} {...track} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
