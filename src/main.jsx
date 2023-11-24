5c5bd6565245691b823c77c3fdf9db2a15742ba0
Daniel <danielmuratov2003@gmail.com>
2023-11-24 17:39:35 +0600

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import AudioProviderContext from './context/AudioContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <AudioProviderContext>
     <App />
-  </React.StrictMode>
+  </AudioProviderContext>
 );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,42 @@
-import tracksList from '../../assets/tracksList';
-import style from './mainPage.module.scss';
+import { useState } from 'react';
 import Track from '../../components/Track/Track';
+import { Input } from '@mui/material';
+
+import tracksList from '../../assets/tracksList';
+
+import style from './mainPage.module.scss';
+
+const runSearch = (query) => {
+  if (!query) {
+    return tracksList;
+  }
+
+  const lowerCaseQuery = query.toLowerCase();
+
+  return tracksList.filter(
+    (track) =>
+      track.title.toLowerCase().includes(lowerCaseQuery) ||
+      track.artists.toLowerCase().includes(lowerCaseQuery)
+  );
+};
 
 const MainPage = () => {
+  const [tracks, setTracks] = useState(tracksList);
+
+  const handleChange = (event) => {
+    const foundTracks = runSearch(event.target.value);
+    setTracks(foundTracks);
+  };
+
   return (
     <div className={style.search}>
-      <>Поиск треков</>
+      <Input
+        className={style.input}
+        placeholder="Поиск треков"
+        onChange={handleChange}
+      />
       <div className={style.list}>
-        {tracksList.map((track) => (
+        {tracks.map((track) => (
           <Track key={track.id} {...track} />
         ))}
       </div>
